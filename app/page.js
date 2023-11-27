62fe425345a499298ef833c1716afda9a3ffba72
lark1987 <bosworth210920@gmail.com>
2023-11-27 17:56:36 +0800

    1127-1756

--- app/page.js
+++ app/page.js
@@ -6,7 +6,23 @@ export default function Home() {
 
   return (
     <>
-      <Lobby/>
+    
+    <div className='pic-box'>
+    <img className='homePic pic-rocket' src='/homepage/home-rocket.png' alt="pic-rocket" />
+    <img className='homePic pic-star' src='/homepage/home-star.png' alt="pic-star" />
+    <img className='homePic pic-balloon' src='/homepage/home-balloon.png' alt="pic-balloon" />
+    <img className='homePic pic-bling' src='/homepage/home-bling.png' alt="pic-bling" />
+    <img className='homePic pic-earth' src='/homepage/home-earth.png' alt="pic-earth" />
+    <br/><br/><br/><br/>
+    <img className='pic-rocket-800' src='/homepage/home-rocket-2.png' alt="pic-rocket" />
+    <Lobby/>
+    <img className='pic-welcome' src='/homepage/home-welcome.png' alt="pic-welcome" />
+    <br/><br/><br/><br/>
+
+    
+    
+
+    </div>
     </>
 
   )
