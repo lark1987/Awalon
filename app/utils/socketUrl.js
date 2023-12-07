0b61d612e3bd28bb6bc11d93081336d96e97ddfd
lark1987 <bosworth210920@gmail.com>
2023-12-07 10:19:16 +0800

    test - 12071019

--- app/utils/socketUrl.js
+++ app/utils/socketUrl.js
@@ -1,6 +1,6 @@
 
 
-export const socketUrl = 'http://localhost:4000/';
+// export const socketUrl = 'http://localhost:4000/';
 
-// export const socketUrl = 'https://www.lark1987.online/';
+export const socketUrl = 'https://www.lark1987.online/';
 
