44359a1766d2c09a669b36587bc78d17b41bb23a
lark1987 <127852077+lark1987@users.noreply.github.com>
2024-02-19 14:49:37 +0800

    change socket.io URL

--- app/utils/socketUrl.js
+++ app/utils/socketUrl.js
@@ -2,5 +2,7 @@
 
 // export const socketUrl = 'http://localhost:4000/';
 
-export const socketUrl = 'https://www.lark1987.online/';
+// export const socketUrl = 'https://www.lark1987.online/';
+
+export const socketUrl = 'https://lark1987.online/';
 
